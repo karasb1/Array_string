b03ad5e385b1d9b130969a771ecc9951a3163fa5
karasb1 <bohdankaras456@gmail.com>
2025-01-26 15:22:50 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+            }
+            Console.WriteLine("Array: ");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            int odd = 0;
+            int even = 0;
+            int unique = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isUnique = true;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j] && i != j)
+                    {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique)
+                {
+                    unique++;
+                }
+            }
+            Console.WriteLine("Odd: " + odd);
+            Console.WriteLine("Even: " + even);
+            Console.WriteLine("Unique: " + unique);
         }
     }
 }
