efbcca4a5e0b2cece627f3892e14903fb7658b43
karasb1 <bohdankaras456@gmail.com>
2025-01-26 19:21:32 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            string sentence = Convert.ToString(Console.ReadLine());
+            string[] words = sentence.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+
+            }
         }
     }
 }
