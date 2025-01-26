8b149bd5d714446374e057b069adde1f73c4fb00
karasb1 <bohdankaras456@gmail.com>
2025-01-26 19:23:53 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            string sentence = Console.ReadLine();
+            int vowels = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u')
+                {
+                    vowels++;
+                }
+            }
+            Console.WriteLine("Vowels : " + vowels);
         }
     }
 }
