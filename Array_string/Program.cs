88ce00d3a3c5bcab832b4886ddb5df1e92f27390
karasb1 <bohdankaras456@gmail.com>
2025-01-26 19:16:41 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            string sentence = Convert.ToString(Console.ReadLine());
+            int wordCount = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == ' ')
+                {
+                    wordCount++;
+                }
+            }
+            Console.WriteLine("Word count: " + (wordCount + 1));
         }
     }
 }
