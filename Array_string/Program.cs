6321f23d44ff5ac6fed3011f3d9c45a328253c21
karasb1 <bohdankaras456@gmail.com>
2025-01-26 19:30:25 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter sentence: ");
+            string sentence = Console.ReadLine();
+            Console.WriteLine("Enter word: ");
+            string word = Console.ReadLine();
+            int count = 0;
+            for(int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == word[0])
+                {
+                    bool isWord = true;
+                    for (int j = 1; j < word.Length; j++)
+                    {
+                        if (sentence[i + j] != word[j])
+                        {
+                            isWord = false;
+                            break;
+                        }
+                    }
+                    if (isWord)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Number of word in sentence: " + count);
         }
     }
 }
