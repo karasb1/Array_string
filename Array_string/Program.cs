05670a0e1b2506c36d9b08ed828d9e1eb0ad0f59
karasb1 <bohdankaras456@gmail.com>
2025-01-26 18:56:05 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int[] array = new int[10];
+            int parameter;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+            Console.WriteLine("Enter parameter: ");
+            parameter = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Array: ");
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Number of elements less than the parameter: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < parameter)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
         }
     }
 }
