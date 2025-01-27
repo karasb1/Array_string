8dbee02c53f8dfa663ebdad13fcbb39f55dfc0ed
karasb1 <bohdankaras456@gmail.com>
2025-01-27 22:36:09 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int[,] array = new int[5,5];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(-100, 100);
+                }
+            }
+            int min = array[0, 0];
+            int max = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > min && array[i, j] < max)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Sum = " + sum);
         }
     }
 }
