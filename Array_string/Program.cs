b6999952b1327d17029b1632618091f8dace0ca5
karasb1 <bohdankaras456@gmail.com>
2025-02-14 12:31:34 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace Array_string
+﻿using System.Collections.Specialized;
+
+namespace Array_string
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string expression = Console.ReadLine();
+            int result = 0;
+            int number = 0;
+            bool operation = true;
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+                if (char.IsDigit(c))
+                {
+                    number = number * 10 + (c - '0');
+                }
+                else
+                {
+                    if (operation)
+                    {
+                        result += number;
+                    }
+                    else
+                    {
+                        result -= number;
+                    }
+                    number = 0;
+                    if (c == '+')
+                    {
+                        operation = true;
+                    }
+                    else if (c == '-')
+                    {
+                        operation = false;
+                    }
+                }
+                if (operation)
+                {
+                    result += number;
+                }
+                else
+                {
+                    result -= number;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
