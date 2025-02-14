c76c919420347dddc098bc5fd253afa38299f659
karasb1 <bohdankaras456@gmail.com>
2025-02-14 12:17:37 +0200

    .

--- Array_string/Program.cs
+++ Array_string/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            string text = Console.ReadLine();
+            bool newsentence = true;    
+            foreach(char a in text)
+            {
+                if (newsentence && char.IsLetter(a))
+                {
+                    Console.Write(char.ToUpper(a));
+                    newsentence = false;
+                }
+                else
+                {
+                    Console.Write(a);
+                }
+                if (a == '.' || a == '?' || a == '!')
+                {
+                    newsentence = true;
+                }
+            }
         }
     }
 }
